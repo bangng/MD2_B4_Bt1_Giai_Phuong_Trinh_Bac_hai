f51e9bfb5ce298377fb286b985a27ff35b1902cc
bangng <montersb@gmail.com>
2022-07-28 10:51:11 +0700

    oop

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        QuadraticEquation quadraticEquation = new QuadraticEquation();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a: ");
+        double a = scanner.nextDouble();
+        while (a < 0){
+            System.out.println("Return enter a, greater than");
+            a = scanner.nextDouble();
+        }
+        quadraticEquation.setA(a);
+
+        System.out.println("Enter b: ");
+        double b = scanner.nextDouble();
+        quadraticEquation.setB(b);
+        System.out.println("Enter c: ");
+        double c = scanner.nextDouble();
+        quadraticEquation.setC(c);
+        double delta = quadraticEquation.getDiscriminant();
+        if (delta < 0){
+            System.out.println("Phương trình vô nghiệm:");
+        }else if (delta == 0){
+            System.out.println("Phương trình có nghiệm kép: ");
+
+            System.out.println("x =" +quadraticEquation.getRoot());
+        }else {
+            System.out.println("Phương trình có 2 nghiệm: ");
+            System.out.println("x1 =" +quadraticEquation.getRoot1());
+            System.out.println("x2 =" +quadraticEquation.getRoot2());
+        }
+    }
+}
